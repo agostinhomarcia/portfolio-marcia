7a5729bcba551566c4c2612ebb71b0f833186d21
Marcia Agostinho <marciaagostinho27@gmail.com>
2023-01-19 15:19:58 -0300

    add efeito

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,17 @@ function displayAbout() {
     let display = document.getElementById('about');
     display.classList.toggle('active');
 }
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
